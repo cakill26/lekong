e427f8dec3c966b13bba4bcabd515cf27211caab
envy26 <113749002+cakill26@users.noreply.github.com>
2023-03-10 08:53:05 +0700

    Add files via upload

---
+++ websocket/edu.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : Adit Ardiansyah
+# (C) Copyright 2022
+# =========================================
+
+clear
+date
+echo ""
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-tls https://${Server_URL}/websocket/ws-tls.py
+chmod +x /usr/local/bin/ws-tls
+
+# Installing Service
+cat > /etc/systemd/system/ws-tls.service << END
+[Unit]
+Description=Python Proxy Mod By geovpn
+Documentation=https://t.me/geovpn
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-tls
+systemctl restart ws-tls
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-nontls https://${Server_URL}/websocket/ws-nontls.py
+chmod +x /usr/local/bin/ws-nontls
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=Python Proxy Mod By geovpn
+Documentation=https://t.me/geovpn
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 80
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-nontls
+systemctl restart ws-nontls
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-ovpn https://${Server_URL}/websocket/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Python Proxy Mod By geovpn
+Documentation=https://t.me/geovpn
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
